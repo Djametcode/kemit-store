afadd063469f76f21dd371301739a901e43c0f3b
djamet <kampretsiji1@gmail.com>
2023-10-10 17:16:28 +0700

    changes

---
+++ frontend/k-store/src/App.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export default function App() {
+  return <div className=" bg-black"></div>;
+}
---
+++ frontend/k-store/src/components/auth.tsx
@@ -0,0 +1,7 @@
+export const AuthComponent = () => {
+  return (
+    <div className=" bg-black h-screen text-white font-mono p-10">
+      <h1>Hello</h1>
+    </div>
+  );
+};
